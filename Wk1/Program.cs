68e12a38a86ea8a7bd9c6dee89701dc09530b02a
Roma <roma.ethereal@gmail.com>
2025-09-29 15:09:07 +0300

    [Async] Remove sync-over-async + CancellationToken

--- Wk1/Program.cs
+++ Wk1/Program.cs
@@ -1,8 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
-builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddHttpContextAccessor();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
@@ -16,8 +15,17 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.UseAuthorization();
 
-app.MapControllers();
+app.MapGet("/user", async (IHttpContextAccessor httpContext, CancellationToken ct) => {
+    var users = await GetUsersFromDbAsync(httpContext, ct);
+    return users;
+});
 
 app.Run();
+
+async Task<List<string>> GetUsersFromDbAsync(IHttpContextAccessor httpContext, CancellationToken ct)
+{
+    await Task.Delay(1000, ct); // імітація доступу до БД
+    return new List<string> { "Alice", "Bob", "Charlie" };
+}
+
